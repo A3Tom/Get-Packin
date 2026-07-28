f4cdca49d6dcc5a31a65db9a6a8880ce5856cf1f
Tom Smith <Tom_Esq@yahoo.com>
2026-07-28 22:22:14 +0100

    Got pieces printin

---
+++ enums.cs
@@ -0,0 +1,10 @@
+public enum Piece
+{
+    RED,
+    GREEN,
+    BLUE,
+    WHITE,
+    BLACK,
+    PURPLE,
+    PINK
+}
--- program.cs
+++ program.cs
@@ -1,6 +1,19 @@
 ﻿ulong board = 0b00000000_00000000_00000000;
+Dictionary<Piece, ulong> pieces = new()
+{
+    { Piece.RED,        0b00001111 },
+    { Piece.BLUE,       0b00000101_00000111 },
+    { Piece.GREEN,      0b00000010_00000111 },
+    { Piece.PINK,       0b00000001_00001111 },
+    { Piece.PURPLE,     0b00000100_00001111 },
+};
 
-void printBoard(ulong board)
+Console.WriteLine("~~~ board ~~~");
+printBoard(board);
+Console.WriteLine("\n~~~ pieces ~~~");
+printPieces(pieces);
+
+static void printBoard(ulong board)
 {
     const int width = 8;
     const int mask = 0b1;
@@ -20,4 +33,11 @@ void printBoard(ulong board)
     }
 }
 
-printBoard(board);
+static void printPieces(Dictionary<Piece, ulong> pieces)
+{
+    foreach (var piece in pieces)
+    {
+        Console.WriteLine($"\n :: {Enum.GetName(piece.Key)} :: ");
+        printBoard(piece.Value);
+    }
+}
