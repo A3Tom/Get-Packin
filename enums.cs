37f83eb4ad67056e2026fe8e8f2eeaec7b245621
Tom Smith <Tom_Esq@yahoo.com>
2026-07-28 22:40:22 +0100

    Fancy printin pieces

--- enums.cs
+++ enums.cs
@@ -5,6 +5,6 @@ public enum Piece
     BLUE,
     WHITE,
     BLACK,
-    PURPLE,
+    YELLOW,
     PINK
 }
--- program.cs
+++ program.cs
@@ -1,11 +1,16 @@
-﻿ulong board = 0b00000000_00000000_00000000;
+﻿using System.Diagnostics;
+
+const char EMPTY_SPACE = '·';
+const char BLOCK = '█';
+
+const ulong board = 0b00000000_00000000_00000000;
 Dictionary<Piece, ulong> pieces = new()
 {
     { Piece.RED,        0b00001111 },
     { Piece.BLUE,       0b00000101_00000111 },
     { Piece.GREEN,      0b00000010_00000111 },
     { Piece.PINK,       0b00000001_00001111 },
-    { Piece.PURPLE,     0b00000100_00001111 },
+    { Piece.YELLOW,     0b00000100_00001111 },
 };
 
 Console.WriteLine("~~~ board ~~~");
@@ -20,13 +25,14 @@ static void printBoard(ulong board)
 
     for (int row = 0; row < 3; row++)
     {
-        Console.Write($"[{row}]:");
+        Console.Write($"[{row}] ");
         
         for(int cell = 0; cell < width; cell++)
         {
             var idx = (row * width) + cell;
             var hing = (board >> idx) & mask;
-            Console.Write($"{hing}|");
+            var outputChar = hing == 1 ? BLOCK : EMPTY_SPACE;
+            Console.Write($"{outputChar} ");
         }
 
         Console.WriteLine();
@@ -38,6 +44,43 @@ static void printPieces(Dictionary<Piece, ulong> pieces)
     foreach (var piece in pieces)
     {
         Console.WriteLine($"\n :: {Enum.GetName(piece.Key)} :: ");
-        printBoard(piece.Value);
+        printPiece(piece.Key, piece.Value);
     }
 }
+
+static void printPiece(Piece piece, ulong pieceBlocks)
+{
+
+    const int width = 8;
+    const int mask = 0b1;
+
+    for (int row = 0; row < 3; row++)
+    {
+        Console.Write($"[{row}] ");
+        
+        for(int cell = 0; cell < width; cell++)
+        {
+            var idx = (row * width) + cell;
+            var hing = (pieceBlocks >> idx) & mask;
+            var outputChar = hing == 1 ? BLOCK : EMPTY_SPACE;
+            Console.ForegroundColor = GetPieceConsoleColour(piece);
+            Console.Write($"{outputChar}");
+            Console.ResetColor();
+        }
+
+        Console.WriteLine();
+    }
+
+}
+
+static ConsoleColor GetPieceConsoleColour(Piece piece) => piece switch
+{
+    Piece.RED => ConsoleColor.Red,
+    Piece.GREEN => ConsoleColor.Green,
+    Piece.BLUE => ConsoleColor.Blue,
+    Piece.WHITE => ConsoleColor.White,
+    Piece.BLACK => ConsoleColor.Black,
+    Piece.YELLOW => ConsoleColor.Yellow,
+    Piece.PINK => ConsoleColor.Magenta,
+    _ => ConsoleColor.White,
+};
