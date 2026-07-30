f365373650a7cfdf6c0eeaabd512080c838420b6
Tom Smith <Tom_Esq@yahoo.com>
2026-07-30 22:18:39 +0100

    Got normalization working

--- helpers/MatrixHelper.cs
+++ helpers/MatrixHelper.cs
@@ -1,20 +1,39 @@
 public static class MatrixHelper
 {
-    public static ulong RotatePiece(ulong pieceBlocks, int pieceHeight, int pieceWidth)
+    public static ulong RotatePiece(GameConfig gameConfig, ulong pieceBlocks)
     {
         ulong result = 0;
 
-        for (int row = pieceHeight -1; row >= 0; row--)
+        for (int row = gameConfig.PieceHeight -1; row >= 0; row--)
         {
-            for (int cell = 0; cell < pieceWidth; cell++)
+            for (int cell = 0; cell < gameConfig.PieceWidth; cell++)
             {
-                var idx = (cell * pieceWidth) + row;
+                var idx = (cell * gameConfig.PieceWidth) + row;
                 var cellValue = (pieceBlocks >> idx) & BitHelper.KENOBI;
                 if (cellValue == 1)
-                    result |= 1u << (((row + 1) * pieceWidth) - (cell + 1));
+                    result |= 1u << (((row + 1) * gameConfig.PieceWidth) - (cell + 1));
             }
         }
 
         return result;
+    }
+
+    public static ulong NormalizePieceBlocks(GameConfig gameConfig, ulong pieceBlocks)
+    {
+        ulong rowMask = 32u-1;
+
+        while((pieceBlocks & rowMask) == 0)
+            pieceBlocks >>= gameConfig.PieceHeight;
+    
+        pieceBlocks = RotatePiece(gameConfig, pieceBlocks);
+
+        while((pieceBlocks & rowMask) == 0)
+            pieceBlocks >>= gameConfig.PieceHeight;
+        
+        pieceBlocks = RotatePiece(gameConfig, pieceBlocks);
+        pieceBlocks = RotatePiece(gameConfig, pieceBlocks);
+        pieceBlocks = RotatePiece(gameConfig, pieceBlocks);
+
+        return pieceBlocks;
     }
 }
--- program.cs
+++ program.cs
@@ -1,27 +1,65 @@
-﻿var gameConfig = new GameConfig()
+﻿using System.Security.Cryptography.X509Certificates;
+
+var gameConfig = new GameConfig()
 {
     BoardHeight = 5,
     BoardWidth = 10,
     PieceHeight = 5,
-    PieceWidth = 5  
+    PieceWidth = 5
 };
 gameConfig.Board = InitBoard(gameConfig);
 
 Dictionary<Piece, ulong> _pieces = new()
 {
     { Piece.RED,        0b01111 },
-    { Piece.BLUE,       0b00101_00111 },
-    { Piece.GREEN,      0B00010_00111 },
-    { Piece.PINK,       0b00001_01111 },
-    { Piece.YELLOW,     0b00100_01111 },
-    { Piece.WHITE,      0b00110_00011 },
+    { Piece.BLUE,       0b00000_01010_01110_00000 },
+    { Piece.GREEN,      0B00000_00010_00111_00000 },
+    { Piece.PINK,       0b00000_00001_01111_00000 },
+    { Piece.YELLOW,     0b00000_00100_01111_00000 },
+    { Piece.WHITE,      0b00000_00110_00011_00000 },
 };
 
-Dictionary<Piece, ulong[]> _piecePermutations = [];
+Dictionary<Piece, ulong[]> _piecePermutations = _pieces.ToDictionary(
+    x => x.Key,
+    v => GetPiecePermutations(gameConfig, v.Value)
+);
 
-Console.WriteLine("~~~ board ~~~");
-PrintHelper.PrintBoard(gameConfig);
-Console.WriteLine("\n~~~ pieces ~~~");
-PrintHelper.PrintPieces(gameConfig, _pieces);
+PrintAllPiecePermutations(gameConfig, _piecePermutations);
+
+static void PrintAllPiecePermutations(GameConfig gameConfig, Dictionary<Piece, ulong[]> piecePermutations)
+{
+    foreach (var piece in piecePermutations)
+    {
+        Console.WriteLine($"\n ~ {Enum.GetName(piece.Key)} ~ ");
+
+        foreach (var perm in piece.Value)
+        {
+            Console.WriteLine();
+            PrintHelper.PrintPiece(gameConfig, piece.Key, perm);
+        }
+
+    }
+}
+
+static ulong[] GetPiecePermutations(GameConfig gameConfig, ulong pieceBlocks)
+{
+    var result = new List<ulong>();
+    result.AddRange(
+        GetPieceRotationPermutations(gameConfig, pieceBlocks)
+    );
+
+    return [.. result.ToHashSet()];
+}
+
+static IEnumerable<ulong> GetPieceRotationPermutations(GameConfig gameConfig, ulong pieceBlocks)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < i; j++)
+            pieceBlocks = MatrixHelper.RotatePiece(gameConfig, pieceBlocks);
+        
+        yield return MatrixHelper.NormalizePieceBlocks(gameConfig, pieceBlocks);
+    }
+}
 
 static ulong InitBoard(GameConfig gameConfig) => 0u << (gameConfig.BoardHeight * gameConfig.BoardWidth);
